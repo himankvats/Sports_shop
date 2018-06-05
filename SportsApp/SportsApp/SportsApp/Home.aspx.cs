c9342c43667fbc008213f7e2b77d381b9c695506
himankvats <himankvats@gmail.com>
2018-06-05 04:30:39 -0400

    Changed repositary from Project to new one

---
+++ SportsApp/SportsApp/SportsApp/Home.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Runtime;
+using Newtonsoft.Json;
+namespace SportsApp
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string searchstring = TextBox2.Text;
+            string srearchby = DropDownList1.SelectedValue;
+            string searchQuery;
+
+            string Connectionstring = ConfigurationManager.ConnectionStrings["LocalDB"].ConnectionString;
+
+            using(SqlConnection con = new SqlConnection(Connectionstring))
+            {
+                if (String.Equals(srearchby, "ProductID"))
+                {
+                    searchQuery = "Select * from [Products] where [Product_Id] like '" + searchstring + "%'";
+                }
+                else
+                {
+                     searchQuery = "Select * from [Products] where [Product_Name] like '%" + searchstring+ "%'";
+                }
+                SqlCommand command = new SqlCommand(searchQuery, con);
+                DataTable SearchTable = new DataTable();
+                con.Open();
+                SearchTable.Load(command.ExecuteReader());
+                if(SearchTable.Rows.Count==0)
+                {
+                    GridView1.Visible = false;
+                    Label1.Visible = true;
+                    Label1.Text = "Sorry couldnt find anything for this search [tip search in warehouse]";
+                }
+                else
+                {
+                    GridView1.Visible = true;
+                    Label1.Visible = false;
+                    GridView1.DataSource = SearchTable;
+                    GridView1.DataBind();
+                }
+
+            }
+
+             
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string searchstring = TextBox2.Text;
+            string srearchby = DropDownList1.SelectedValue;
+
+            InventorySearch.SearchServiceClient client = new InventorySearch.SearchServiceClient();
+            string JsonTable= client.SearchInventory(searchstring, srearchby);
+            DataTable SearchTable = new DataTable();
+               SearchTable=  JsonConvert.DeserializeObject <DataTable>(JsonTable);
+            if (SearchTable.Rows.Count == 0)
+            {
+                GridView1.Visible = false;
+                Label1.Visible = true;
+                Label1.Text = "Sorry couldnt find anything for this search";
+            }
+            else
+            {
+                GridView1.Visible = true;
+                Label1.Visible = false;
+                GridView1.DataSource = SearchTable;
+                GridView1.DataBind();
+            }
+            
+        }
+    }
+}
---
+++ SportsService/SportsInventorySearch/SportsInventorySearch/Service1.svc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+
+namespace SportsInventorySearch
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : ISearchService
+    {
+       
+
+        public string SearchInventory(string SearchString, string SearchBy)
+        {
+            string searchQuery;
+            string Connectionstring = ConfigurationManager.ConnectionStrings["InventoryDB"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(Connectionstring))
+            {
+                if (String.Equals(SearchBy, "ProductID"))
+                {
+                    searchQuery = "Select * from [Products] where [Product_Id] like '" + SearchString + "%'";
+                }
+                else
+                {
+                    searchQuery = "Select * from [Products] where [Product_Name] like '%" + SearchString + "%'";
+                }
+                con.Open();
+                SqlCommand command = new SqlCommand(searchQuery, con);
+                DataTable SearchTable = new DataTable();
+                
+                SearchTable.Load(command.ExecuteReader());
+
+                string JsonTable = JsonConvert.SerializeObject(SearchTable);
+                return JsonTable;
+
+            }
+
+               
+        }
+    }
+}
